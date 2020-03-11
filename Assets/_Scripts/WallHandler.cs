bf3c6c9bf19dc1d9ab7e2f84eed0754fde8e89e0
DylanLoftus <dylanloftus@hotmail.com>
2020-03-11 17:28:36 +0000

    Update WallHandler.cs

--- Assets/_Scripts/WallHandler.cs
+++ Assets/_Scripts/WallHandler.cs
@@ -24,22 +24,18 @@ public class WallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Checking player near");
         CheckPlayerNear();
     }
 
     private void CheckPlayerNear()
     {
-        Debug.Log("called Player Near");
 
         if (player.transform.position.x > sr.gameObject.transform.position.x - 3 && player.transform.position.x < sr.gameObject.transform.position.x + 3)
         {
-            Debug.Log("Player Near");
             playerNear = true;
         }
         else
         {
-            Debug.Log("Player not Near");
             playerNear = false;
         }
 
@@ -47,7 +43,6 @@ public class WallHandler : MonoBehaviour
         {
             if (CheckPassword())
             {
-                Debug.Log("Turn off Wall");
                 sr.gameObject.SetActive(false);
                 lock1 = false; lock2 = false; lock3 = false;
             }
@@ -63,19 +58,16 @@ public class WallHandler : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.I))
         {
             lock1 = true;
-            Debug.Log("Lock 1 set");
         }
 
         if (Input.GetKeyDown(KeyCode.O))
         {
             lock2 = true;
-            Debug.Log("Lock 2 set");
         }
 
         if (Input.GetKeyDown(KeyCode.P))
         {
             lock3 = true;
-            Debug.Log("Lock 3 set");
         }
 
         if (lock1 && lock2 && lock3)

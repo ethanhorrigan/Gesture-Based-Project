438ac0830ebc6888de28363e699c894cbcfb95ff
EthanHorrigan <ethanhorr@gmail.com>
2020-03-12 01:03:24 +0000

    Enemy Death Handler

---
+++ Assets/EnemyDeathHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeathHandler : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// Enemy Death Handler ensures enemys have a "Life Span" so that enemey objects
+    /// are destroyed after a peroid of time, to prevent stacking of unused objects.
+    /// 
+    /// </summary>
+    /// 
+
+    public float lifespan; //a float variable to set how long the enemies lifespan is.
+    void Start()
+    {
+        Destroy(gameObject, lifespan);
+    }
+
+}
--- Assets/_Scripts/EnemyHandler.cs
+++ Assets/_Scripts/EnemyHandler.cs
@@ -10,4 +10,13 @@ public class EnemyHandler : MonoBehaviour
     {
         transform.Translate(Vector2.left * speed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Player>().health--;
+            Destroy(gameObject);
+        }
+    }
 }
